f7a4a1e01b1df5248e499262da953e8a7f83b709
Narma <narma_91@gmail.com>
2023-01-06 20:46:16 +0400

    recursion

---
+++ Задача64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+Recursion(8, 1);
+
+void Recursion(int m, int n)
+{
+    if (m <= n)
+    {
+        System.Console.WriteLine(m);
+        return;
+    }
+    System.Console.Write($"{m}, ");
+    Recursion(m - 1, n);
+}
+AltRecursion(5);
+void AltRecursion(int m)
+{
+    if (m <= 1)
+    {
+        System.Console.WriteLine(m);
+        return;
+    }
+    System.Console.Write($"{m}, ");
+    AltRecursion(m - 1);
+}
+
