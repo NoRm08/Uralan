20b54e3fd8fd2c93b61e5a897af5b6e9e4359412
Narma <narma_91@gmail.com>
2023-01-06 20:51:01 +0400

    summ

---
+++ Задача66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+SumInRangeMN(ReadInt("from"), ReadInt("to"), 0);
+
+void SumInRangeMN(int m, int n, int sum)
+{
+    sum += n;
+    if (n <= m)
+    {
+        System.Console.Write($"{sum}");
+        return;
+    }
+    System.Console.WriteLine($"{m} + {n} = {sum}");
+    SumInRangeMN(m, n - 1, sum);
+}
+int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
